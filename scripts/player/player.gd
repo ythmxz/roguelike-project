9be98ea3b0fc29a0e25492a8b5804d14d58540cd
Yuri Thomaz <ytcarvalho14@gmail.com>
2025-05-16 22:53:47 -0300

    Adiciona Player

---
+++ scripts/player/player.gd
@@ -0,0 +1,17 @@
+class_name Player
+extends CharacterBody2D
+
+
+@export var health_component: HealthComponent = null
+@export var hurtbox_component: HurtboxComponent = null
+@export var movement_component: MovementComponent = null
+@export var animation_component: AnimationComponent = null
+@export var state_machine: StateMachine = null
+
+
+func _ready() -> void:
+	assert(health_component, "A HealthComponent must be selected on Player!")
+	assert(hurtbox_component, "A HurtboxComponent must be selected on Player!")
+	assert(movement_component, "A Movement must be selected on Player!")
+	assert(animation_component, "An Animation must be selected on Player!")
+	assert(state_machine, "A StateMachine must be selected on Player!")
