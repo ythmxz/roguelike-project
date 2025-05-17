412a29ddcb2865cdf673fa99330977d8bfc67cad
Yuri Thomaz <ytcarvalho14@gmail.com>
2025-05-16 22:00:17 -0300

    Adiciona AnimationComponent

---
+++ scripts/components/animation_component.gd
@@ -0,0 +1,45 @@
+class_name AnimationComponent
+extends Node2D
+
+
+@export var sprite: AnimatedSprite2D = null
+
+var mouse_angle: float = 0.0:
+	get = _get_mouse_angle
+
+
+func _ready() -> void:
+	assert(sprite, "An Sprite must be selected!")
+
+
+func _get_mouse_angle() -> float:
+	return rad_to_deg((get_global_mouse_position() - owner.global_position).angle())
+
+
+func play_animation(value: String) -> void:
+
+	if value == sprite.animation:
+		return
+
+	sprite.play(value)
+
+
+func set_flip_direction(direction: Vector2) -> void:
+
+	if direction.x > 0.0:
+		sprite.flip_h = false
+	if direction.x < 0.0:
+		sprite.flip_h = true
+
+
+func set_flip_angle(angle: float) -> void:
+
+	if angle > -90.0 or angle < 90.0:
+		sprite.flip_h = false
+	if angle < -90.0 or angle > 90.0:
+		sprite.flip_h = true
+
+
+func follow_mouse() -> void:
+	look_at(get_global_mouse_position())
+	set_flip_angle(mouse_angle)
